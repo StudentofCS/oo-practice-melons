7252ba2009d0e53c3bcbf23dfd5833d8550f51ed
Luis Minaya <luis.minaya@gmail.com>
2025-02-18 21:32:13 -0500

    Completed harvest.py __init__

---
+++ harvest.py
@@ -0,0 +1,91 @@
+############
+# Part 1   #
+############
+
+
+class MelonType:
+    """A species of melon at a melon farm."""
+
+    def __init__(
+        self, code, first_harvest, color, is_seedless, is_bestseller, name
+    ):
+        """Initialize a melon."""
+
+        self.pairings = []
+
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        
+        # Make seedless and bestseller into booleans
+        if is_seedless.lower() == "seedless":
+            self.is_seedless = True
+        else:
+            self.is_seedless = False
+
+        if is_bestseller.lower() == "bestseller":
+            self.is_bestseller = True
+        else:
+            self.is_bestseller = False
+        
+        self.name = name
+        print(self.name, self.is_bestseller, self.is_seedless,
+              self.color, self.first_harvest, self.code)
+
+        # Fill in the rest
+
+    def add_pairing(self, pairing):
+        """Add a food pairing to the instance's pairings list."""
+
+        # Fill in the rest
+
+    def update_code(self, new_code):
+        """Replace the reporting code with the new_code."""
+
+        # Fill in the rest
+
+
+def make_melon_types():
+    """Returns a list of current melon types."""
+
+    all_melon_types = []
+
+    # Fill in the rest
+
+    return all_melon_types
+
+
+def print_pairing_info(melon_types):
+    """Prints information about each melon type's pairings."""
+
+    # Fill in the rest
+
+
+def make_melon_type_lookup(melon_types):
+    """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
+
+    # Fill in the rest
+
+
+############
+# Part 2   #
+############
+
+
+class Melon:
+    """A melon in a melon harvest."""
+
+    # Fill in the rest
+    # Needs __init__ and is_sellable methods
+
+
+def make_melons(melon_types):
+    """Returns a list of Melon objects."""
+
+    # Fill in the rest
+
+
+def get_sellability_report(melons):
+    """Given a list of melon object, prints whether each one is sellable."""
+
+    # Fill in the rest
